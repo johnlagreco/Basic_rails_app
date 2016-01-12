608ca66e8fa07b9ec4ae644f90dfb15bf10005e5
John LaGreco <johnlagreco@gmail.com>
2016-01-11 23:39:26 -0500

    Modified css, rb & erb files. Added new erb file.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
   def index
   end
+
+  def new
+  	@users = User.new
+  end
+
+  def create
+  	@user = User.new(params[:user])
+  	if @user.save
+  		redirect_to user_path @user
+  	else
+  		redirect_to new_user_path
+  	end
+  end
+  
 end

0ee9f9313baa203aa57d0de37dd7a06beb2743b1
John LaGreco <johnlagreco@gmail.com>
2016-01-12 21:25:44 -0500

    Added erb files & modified index and users controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   def new
@@ -14,5 +19,23 @@ class UsersController < ApplicationController
   		redirect_to new_user_path
   	end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(params[:user])
+    @user.save
+    redirect_to @user
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
+  end
+
   
 end
